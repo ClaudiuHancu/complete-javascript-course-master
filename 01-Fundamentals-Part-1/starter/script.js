7775f5ad94dd892ac19a487c4d7d9ef6a4762cd2
ClaudiuHancu <claudiuhancu@gmail.com>
2021-01-26 13:13:34 +0200

    exercitiul1

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -25,7 +25,7 @@ let job1 = 'programmer';
 let job2 = 'teacher';
 
 console.log(myFirstJob);
-*/
+
 
 let jawascriptIsFun = true;
 console.log(jawascriptIsFun);
@@ -46,3 +46,92 @@ console.log(typeof year);
 
 console.log(typeof null);
 
+
+let age = 30;
+age = 31;
+
+const birthYear = 1991;
+//birthYear = 1990;
+
+//const job;
+
+var job = `programmer`;
+job = `teacher`;
+
+lastName = `Schmedtmann`
+console.log(lastName);
+
+//Math operarators
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+console.log(ageJonas, ageSarah);
+
+console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
+//2**3 este 2 la puterea 3 , 2*2*2
+
+const firstName = 'Jonas';
+const lastName = 'Scmedtmann';
+console.log(firstName + ` ` + lastName);
+
+//Asisignment operators
+let x = 10 + 5;//x=15
+x += 10; //x=x+10 =25
+x *= 4;//x=x*4=100
+x++;//x=x+1
+x--;//x=x-1
+x--;
+console.log(x);
+
+//Comparison operators
+console.log(ageJonas > ageSarah); //> ,<, >=, <=
+console.log(ageSarah >= 18);
+
+const isFullAge = ageSarah >= 18;
+console.log(now - 1991 > now - 2018);
+
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+console.log(now - 1991 > now - 2018);
+
+let x, y;
+x = y = 25 - 10 - 5;//x=y=10, x=10
+console.log(x, y);
+
+const averageAge = (ageJonas + ageSarah) / 2;
+console.log(ageJonas, ageSarah, averageAge);
+
+Mark and John are trying to compare their BMI(Body Mass Index), which is
+calculated using the formula:
+BMI = mass / height ** 2 = mass / (height * height)(mass in kg
+and height in meter).
+Your tasks:
+1. Store Mark's and John's mass and height in variables
+2. Calculate both their BMIs using the formula(you can even implement both
+versions)
+3. Create a Boolean variable 'markHigherBMI' containing information about
+whether Mark has a higher BMI than John.
+Test data:
+§ Data 1: Marks weights 78 kg and is 1.69 m tall.John weights 92 kg and is 1.95
+1.95 m tall.
+§ Data 2: Marks weights 95 kg and is 1.88 m tall.John weights 85 kg and is 1.76m tall.
+GOOD LUCK �
+
+
+const massMark = 78;
+const heightMark = 1.69;
+const massJohn = 95;
+const heightJohn = 1.95;
+
+const massMark = 95;
+const heightMark = 1.88;
+const massJohn = 85;
+const heightJohn = 1.76;
+
+const BMIMark = massMark / heightMark ** 2;
+const BMIJohn = massJohn / (heightJohn * heightJohn);
+const markHigherBMI = BMIMark > BMIJohn;
+
+console.log(BMIMark, BMIJohn, markHigherBMI);
+*/
