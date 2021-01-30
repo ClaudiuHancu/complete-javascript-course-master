5fb1dd97a338f5030ba4f0dcf16e5dd5cf0cf224
ClaudiuHancu <claudiuhancu@gmail.com>
2021-01-30 21:28:25 +0200

    strings

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -134,4 +134,24 @@ const BMIJohn = massJohn / (heightJohn * heightJohn);
 const markHigherBMI = BMIMark > BMIJohn;
 
 console.log(BMIMark, BMIJohn, markHigherBMI);
+
+const firstName = `Jonas`;
+const job = `teacher`;
+const birthYear = `1991`;
+const year = 2037
+
+const jonas = "I'm " + firstName + `,a ` + (year - birthYear) + ` years old ` + job + `!`;
+console.log(jonas);
+
+const jonasNew = `I'm ${firstName} a ${year - birthYear} year old ${job} !`;
+console.log(jonasNew);
+console.log(`Just a regular string...!`);
+
+console.log(`Strings with\n\
+multiple\n\
+lines`);
+
+console.log(`Strings
+multiple
+lines`);
 */
