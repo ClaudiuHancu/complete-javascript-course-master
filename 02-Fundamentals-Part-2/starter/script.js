d99dcd196922765f0b104fb70f69ee17b4445925
ClaudiuHancu <claudiuhancu@gmail.com>
2021-02-14 01:37:32 +0200

    curs 2

---
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,54 @@
+`use strict`;
+
+// let hasDriversLicence = false;
+// const passTest = true;
+
+// if (passTest) hasDriverLicence = true;
+// if (hasDriversLicence) console.log(`I can drrive :D`);
+/*
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) hasDriverLicense = true;
+if (hasDriversLicense) console.log('I can drive :D');
+
+// const interface = `Audio`;
+// const private = 534;
+
+
+function logger() {
+    console.log(`My name is Jonas`)
+}
+// calling / running/ invoking function
+logger();
+logger();
+logger();
+
+function fruitProcesor(apples, oranges) {
+    const juice = `Juice with ${apples} apples and ${oranges} oranges. `;
+    return juice;
+}
+const appleJuice = fruitProcesor(5, 0);
+console.log(appleJuice);
+
+const appleOrangeJuice = fruitProcesor(2, 4);
+console.log(appleOrangeJuice);
+
+const num = Number(`23`);
+
+//fuction declaration
+
+function calcAge1(birtYear) {
+    return 2037 - birtYear;
+}
+
+const age1 = calcAge1(1991);
+console.log(age1);
+
+//function expression
+const calcAge2 = function calcAge1(birtYear) {
+    return 2037 - birtYear;
+}
+const age2 = calcAge2(1991);
+console.log(age1, age2);
+*/
