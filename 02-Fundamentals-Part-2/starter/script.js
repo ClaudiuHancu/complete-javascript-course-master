1be5b0b3fe3bc096f37bda897594d33a31d0f9b9
ClaudiuHancu <claudiuhancu@gmail.com>
2021-03-04 15:48:28 +0200

    reiau

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -239,3 +239,10 @@ jonas.location = `Portugal`;
 
 console.log(`${jonas.firstName}  has ${jonas.friends.length}  friends and his best friend is called ${jonas.friends[0]}`);
 */
+const jonas = {
+    firstName: `Jonas`,
+    lastName: `Schemedtmann`,
+    age: 2037 - 1991,
+    job: `teacher`,
+    friends: [`Michael`, `Peter`, `Steven`]
+};
